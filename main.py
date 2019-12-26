207dfd3b579543b6661ea7cbb3c846fa873ca3dd
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-26 10:15:16 -0500

    Created html macro

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video del producto']
 
 @app.route('/')
 def index():
