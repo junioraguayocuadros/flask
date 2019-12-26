907dc1bb3f15d22a9cf4cda90c158f44b2384ec6
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-26 10:48:06 -0500

    Added error handler 500

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def not_found(error):
     return render_template('404.html', error=error)
 
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
