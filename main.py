6565939a7445ffac43f952d6c00d4f063424fb13
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-26 20:28:33 -0500

    Added app Factory to project

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+from app import create_app
+
+app = create_app()
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video del producto']
 
@@ -17,12 +15,6 @@ todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video del produ
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
