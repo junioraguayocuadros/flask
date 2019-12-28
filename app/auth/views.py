aee4b153b8c6d1d83a1e232f7a0b9538882c1c07
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-27 19:14:41 -0500

    Added signup route in the project

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
 from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash
 
 from app import UserModel
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.forms import LoginForm
 from flask import render_template, url_for, redirect, flash, session
 
@@ -42,6 +43,36 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+            login_user(user)
+
+            flash('Bienvenido')
+
+            return redirect(url_for('hello'))
+
+        else:
+            flash('El usuario ya existe')
+
+    return render_template('signup.html', **context)
 
 
 @auth.route('logout')
